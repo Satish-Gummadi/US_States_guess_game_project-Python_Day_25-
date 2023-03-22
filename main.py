893d3e3c366e9941ca69a150b84ec73193316b06
Satish <satishgummadi4@gmail.com>
2023-03-22 18:54:33 +0530

    Added the prompt to intake user's guess using screen object

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ import turtle
 screen = turtle.Screen()
 screen.title('Guess the U.S. States name')
 image = 'blank_states_img.gif'
-
 # turtle shape can be circle, square etc, but it has no specific shape like our image
 # so we will first add the image using addshape
 screen.addshape(image)
-
 # now we will use this image to define the shape of our turtle
 turtle.shape(image)
+
+answer_state = screen.textinput(title='Guess the state',prompt="What's the name of the other state?")
+# print(answer_state)
 
 screen.exitonclick()
