6d2f7d18b36e8e419cd7e149a76c140ab390819f
Satish <satishgummadi4@gmail.com>
2023-03-26 20:45:08 +0530

    Added while loop for the game to continue. Added exit to stop loop. Added code to return the states not guessed in red color

--- locate_states_coordinates.py
+++ locate_states_coordinates.py
@@ -1,17 +1,31 @@
-# this code will return the coordinates of the state when clicked on the US map
-# the values of the same have been added to 50_states.csv file corresponding to its state name
-import turtle
+# # this code will return the coordinates of the state when clicked on the US map
+# # the values of the same have been added to 50_states.csv file corresponding to its state name
+# import turtle
+#
+# screen = turtle.Screen()
+# screen.title('Guess the U.S. States name')
+# image = 'blank_states_img.gif'
+# screen.addshape(image)
+# turtle.shape(image)
+#
+# def get_mouse_click_coor(x,y):
+#     print(x,y)
+#
+# turtle.onscreenclick(get_mouse_click_coor)
+#
+# # this will keep our screen open even under clicks unlike screen.exitonclick() command
+# turtle.mainloop()
 
-screen = turtle.Screen()
-screen.title('Guess the U.S. States name')
-image = 'blank_states_img.gif'
-screen.addshape(image)
-turtle.shape(image)
+import pandas as pd
 
-def get_mouse_click_coor(x,y):
-    print(x,y)
+fd = pd.read_csv('50_states.csv')
 
-turtle.onscreenclick(get_mouse_click_coor)
-
-# this will keep our screen open even under clicks unlike screen.exitonclick() command
-turtle.mainloop()
+for i in fd.state:
+    print(i)
+#
+# print(fd.loc[fd['state'] == 'Ohio'])
+#
+# x = fd.loc[fd['state'] == 'Ohio'].y
+#
+#
+# print(x.iloc[0])
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 import turtle
+import pandas as pd
 
 screen = turtle.Screen()
 screen.title('Guess the U.S. States name')
@@ -10,7 +11,44 @@ screen.addshape(image)
 # now we will use this image to define the shape of our turtle
 turtle.shape(image)
 
-answer_state = screen.textinput(title='Guess the state',prompt="What's the name of the other state?")
-# print(answer_state)
+df = pd.read_csv('50_states.csv')
+guessed_set = []
+t = turtle.Turtle()
+t.penup()
 
-screen.exitonclick()
+game_is_on = True
+score = 0
+while score < 50:
+    answer_state = screen.textinput(title=f'{score}/50 Guess the state',prompt="What's the name of the other state?")
+    answer_state = answer_state.title()
+    # print(answer_state)
+    if answer_state == 'Exit':              #to exit the screen when we are done
+        break
+
+    for state in df.state:
+        if answer_state in guessed_set:
+            break
+        elif answer_state == state:
+            score += 1
+            guessed_set.append(answer_state)
+            t.hideturtle()
+            x = df[df.state == answer_state].x
+            y = df[df.state == answer_state].y
+            t.goto(x.item(),y.item())
+            t.write(answer_state,align='center')
+    # print(score)
+
+# now to print all the states which we have not guessed
+for state in df.state:
+    t.speed(7)
+    if state not in guessed_set:
+        x = df[df.state == state].x
+        y = df[df.state == state].y
+        t.goto(x.item(),y.item())
+        t.color('red')
+        t.write(state,align='center')
+
+print(f'Your score is {score}/50')
+
+turtle.mainloop()
+# turtle.exitonclick()
